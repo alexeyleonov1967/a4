926a2c53f9ecd8b2750a1e89426a68bfefa8e10b
Alexey Leonov <leonov_ai@spsr.ru>
2016-11-25 15:31:07 +0300

    Задание номер 8. Оптимизация тестов

--- addressbook-web-tests/src/test/java/ru/stqa/a4/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/a4/addressbook/tests/TestBase.java
@@ -11,7 +11,7 @@ import ru.stqa.a4.addressbook.appmanager.ApplicationManager;
 public class TestBase {
 
   // Internet Explorer (IE)
-  protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
   @BeforeMethod
   public void setUp() throws Exception {
